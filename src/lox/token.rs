8130f04be42bbdf54bc8666836d4a71f2649a08c
fujianbang <fujianbang0ops@gmail.com>
2023-08-02 00:16:15 +0800

    update

--- src/lox/token.rs
+++ src/lox/token.rs
@@ -71,7 +71,6 @@ pub struct Token {
     kind: TokenType,
     lexeme: String,
     line: usize,
-    line_offset: usize,
 }
 
 impl Token {
@@ -79,20 +78,18 @@ impl Token {
         kind: TokenType,
         lexeme: String,
         line: usize,
-        line_offset: usize,
     ) -> Self {
         Self {
             kind,
             lexeme,
             line,
-            line_offset,
         }
     }
 }
 
 impl ToString for Token {
     fn to_string(&self) -> String {
-        format!("(Type: {:?}, Line: {}, Offset: {})", self.kind, self.line, self.line_offset)
+        format!("(Type: {:?}, Lexeme: {}, Line: {})", self.kind, self.lexeme, self.line)
     }
 }
 
@@ -106,8 +103,7 @@ mod test_case {
             TokenType::LEFT_BRACE,
             String::from("{"),
             10,
-            0,
         );
-        assert_eq!(token.to_string(), String::from("(Type: LEFT_BRACE, Line: 10, Offset: 0)"))
+        assert_eq!(token.to_string(), String::from("(Type: LEFT_BRACE, Lexeme: { Line: 10)"))
     }
 }
